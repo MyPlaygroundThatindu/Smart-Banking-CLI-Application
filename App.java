5f84520d8722448df1b53584f0d185c162724dab
Tharindu Thennakoon <chanaka152@gmail.com>
2023-08-16 10:34:09 +0530

    Constants created

--- App.java
+++ App.java
@@ -3,6 +3,27 @@ import java.util.Scanner;
 public class App{
     private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        
+        final String CLEAR = "\033[H\033[2J";
+        final String COLOR_BLUE_BOLD = "\033[34;1m";
+        final String COLOR_RED_BOLD = "\033[31;1m";
+        final String COLOR_GREEN_BOLD = "\033[33;1m";
+        final String RESET = "\033[0m";
+
+        final String DASHBOARD = "Welcome to Smart Banking";
+        final String CREATE_NEW_ACCOUNT = "Open New Account";
+        final String DEPOSITS = "Deposit Money";
+        final String WITHDROWAL = "Withdraw Money";
+        final String TRANSFER = "Transfer Money";
+        final String CHECK_ACCOUNT_BALANCE = "Check Account Balance";
+        final String DELETE_ACCOUNT = "Drop Existing Account";
+
+        final String ERROR_MSG = String.format("\t%s%s%s\n", COLOR_RED_BOLD, "%s", RESET);
+        final String SUCCESS_MSG = String.format("\t%s%s%s\n", COLOR_GREEN_BOLD, "%s", RESET);
+
+        String[] accountId= new String[0];
+        String[] custNames = new String[0];
+        int[] depositAmmunt = new int[0];
+
+        String screen = DASHBOARD;
     }
 }
